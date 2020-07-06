356adb4c50d4d172d0fba3086a4ab377926839ee
j-avila <jose.leon.avila@gmail.com>
2020-07-05 22:45:21 -0400

    envvar settted

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,6 @@ let urlDB
 
 process.env.NODE_ENV === "dev"
 	? (urlDB = "mongodb://localhost:27017/cafe")
-	: (urlDB =
-			"mongodb+srv://jlavila13:o4BU2s5nlShIaeND@cluster0.l40uu.mongodb.net/cafe?retryWrites=true&w=majority")
+	: (urlDB = process.env.NODE_URI)
 
 process.env.URLDB = urlDB
